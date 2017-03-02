39126a0fffd4d3edd468d50988857a20f6404dc0
MTBoren <technology@ddayinc.com>
2017-03-02 10:41:53 -0500

    remove commented-out items

--- Unity-Powershell/Public/Get-UnityLUN.ps1
+++ Unity-Powershell/Public/Get-UnityLUN.ps1
@@ -47,13 +47,7 @@ Function Get-UnityLUN {
       Write-Verbose "Processing Session: $($sess.Server) with SessionId: $($sess.SessionId)"
 
       If ($Sess.TestConnection()) {
-
-        # $StorageResource = Get-UnityStorageResource -Session $Sess -Type 'lun'
-
-        # If ($StorageResource) {
-          # $ResultCollection += Get-UnityLUNResource -Session $Sess -ID $StorageResource.luns.id
           $ResultCollection += Get-UnityLUNResource -Session $Sess
-        # }
       } else {
         Write-Host "You are no longer connected to EMC Unity array: $($Sess.Server)"
       }
